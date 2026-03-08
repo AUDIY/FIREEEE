dc692962cbabba7b1a709075a5d8993e5528d42f
AUDIY <96096729+AUDIY@users.noreply.github.com>
2026-03-09 00:42:30 +0900

    Add FIREEEE_DCLK_EDGE_DET.v and related files

---
+++ 01_FIREEEE_DCLK_EDGE_DET/FIREEEE_DCLK_EDGE_DET_tb.sv
@@ -0,0 +1,202 @@
+/*-----------------------------------------------------------------------------
+* FIREEEE_DCLK_EDGE_DET_tb.sv
+*
+* Testbench for FIREEEE_DCLK_EDGE_DET.v
+*
+* Version: 0.01
+* Author : AUDIY
+* Date   : 2026/03/09
+*
+* License
+--------------------------------------------------------------------------------
+| Copyright AUDIY 2026.                                                        |
+|                                                                              |
+| This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
+|                                                                              |
+| You may redistribute and modify this source and make products using it under |
+| the terms of the CERN-OHL-P v2 (https:/cern.ch/cern-ohl).                    |
+|                                                                              |
+| This source is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,          |
+| INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A         |
+| PARTICULAR PURPOSE. Please see the CERN-OHL-P v2 for applicable conditions.  |
+--------------------------------------------------------------------------------
+*
+-----------------------------------------------------------------------------*/
+`default_nettype none
+
+module FIREEEE_DCLK_EDGE_DET_tb ();
+
+    timeunit 1ns / 1ps;
+
+    localparam RESET_EN        = 1'b1;
+    localparam ASYNC_RESET_EN  = 1'b1;
+    localparam DATA_BIT_WIDTH  = 8;
+    localparam IN_REG_EN       = 1'b1;
+    localparam OUT_REG_EN      = 1'b1;
+    localparam CLK_COUNT_WIDTH = 10;
+
+    reg                                 CLK_I   = 1'b0;
+    reg                                 DCLK_I  = 1'b0;
+    reg signed [(DATA_BIT_WIDTH - 1):0] DATA_I  = {(DATA_BIT_WIDTH){1'b0}};
+    reg                                 N_RST_I = 1'b0;
+
+    wire                                 DCLK_O;
+    wire signed [(DATA_BIT_WIDTH - 1):0] DATA_O;
+    wire                                 POS_DET_O;
+    wire                                 NEG_DET_O;
+
+    reg     unsigned [(CLK_COUNT_WIDTH - 1):0] CLK_COUNT = '0;
+    
+    integer unsigned urandom = '0;
+    integer signed   random  = '0;
+
+    /* Instantiation */
+    generate
+        if (RESET_EN == 1'b0) begin: dut_noreset
+            /* w/o Reset */
+            FIREEEE_DCLK_EDGE_DET #(
+                .RESET_EN      (RESET_EN      ),
+                .ASYNC_RESET_EN(ASYNC_RESET_EN),
+                .DATA_BIT_WIDTH(DATA_BIT_WIDTH),
+                .IN_REG_EN     (IN_REG_EN     ),
+                .OUT_REG_EN    (OUT_REG_EN    )
+            ) dut (
+                .CLK_I    (CLK_I    ),
+                .DCLK_I   (DCLK_I   ),
+                .DATA_I   (DATA_I   ),
+                .N_RST_I  (1'b0     ), // No effect 
+                .DCLK_O   (DCLK_O   ),
+                .DATA_O   (DATA_O   ),
+                .POS_DET_O(POS_DET_O),
+                .NEG_DET_O(NEG_DET_O)
+            );
+        end else if (ASYNC_RESET_EN == 1'b0) begin: dut_syncreset
+            /* w/ Synchronous Reset */
+            reg unsigned [1:0] N_RST_SYNC_I = '0;
+
+            always @(posedge CLK_I) begin
+                N_RST_SYNC_I <= {N_RST_SYNC_I[0], N_RST_I};
+            end 
+
+            FIREEEE_DCLK_EDGE_DET #(
+                .RESET_EN      (RESET_EN      ),
+                .ASYNC_RESET_EN(ASYNC_RESET_EN),
+                .DATA_BIT_WIDTH(DATA_BIT_WIDTH),
+                .IN_REG_EN     (IN_REG_EN     ),
+                .OUT_REG_EN    (OUT_REG_EN    )
+            ) dut (
+                .CLK_I    (CLK_I       ),
+                .DCLK_I   (DCLK_I      ),
+                .DATA_I   (DATA_I      ),
+                .N_RST_I  (N_RST_SYNC_I),
+                .DCLK_O   (DCLK_O      ),
+                .DATA_O   (DATA_O      ),
+                .POS_DET_O(POS_DET_O   ),
+                .NEG_DET_O(NEG_DET_O   )
+            );
+        end else begin: dut_asyncreset
+            /* w/ Asynchronous Reset */
+            wire N_RST_ASYNC_I;
+
+            // Asynchronous Reset Synchronizer.
+            // Please Refer https://github.com/AUDIY/AUDIY_Verilog_IP/tree/main/ARESETN_SYNC
+            ARESETN_SYNC #(
+                .STAGES(2)
+            ) rst_sync (
+                .CLK_I    (CLK_I        ),
+                .ARESETN_I(N_RST_I      ),
+                .RESETN_O (N_RST_ASYNC_I)
+            );
+
+            FIREEEE_DCLK_EDGE_DET #(
+                .RESET_EN      (RESET_EN      ),
+                .ASYNC_RESET_EN(ASYNC_RESET_EN),
+                .DATA_BIT_WIDTH(DATA_BIT_WIDTH),
+                .IN_REG_EN     (IN_REG_EN     ),
+                .OUT_REG_EN    (OUT_REG_EN    )
+            ) dut (
+                .CLK_I    (CLK_I        ),
+                .DCLK_I   (DCLK_I       ),
+                .DATA_I   (DATA_I       ),
+                .N_RST_I  (N_RST_ASYNC_I),
+                .DCLK_O   (DCLK_O       ),
+                .DATA_O   (DATA_O       ),
+                .POS_DET_O(POS_DET_O    ),
+                .NEG_DET_O(NEG_DET_O    )
+            );
+        end
+    endgenerate
+
+    initial begin
+        $dumpfile("FIREEEE_DCLK_EDGE_DET.vcd");
+        $dumpvars(0, FIREEEE_DCLK_EDGE_DET_tb);
+
+        #1000 $finish();
+    end
+
+    /* Clock Generation */
+    initial begin
+        forever begin
+            #3 CLK_I = ~CLK_I;
+        end
+    end
+
+    /* Data Clock Generation */
+    always @(posedge CLK_I) begin
+        CLK_COUNT <= CLK_COUNT + 1'b1;
+
+        DCLK_I <= CLK_COUNT[2];
+
+        urandom <= $urandom_range(1000);
+    end
+
+    always @(negedge CLK_I) begin
+        if (urandom >= 950) begin
+            N_RST_I <= 1'b0;
+        end else begin
+            N_RST_I <= 1'b1;
+        end
+    end
+
+    /* Random Data Generation */
+    always @(negedge DCLK_I) begin
+        random <= $random();
+        DATA_I <= {random[31], random[(DATA_BIT_WIDTH - 2):0]}; // Any Signed Data
+    end
+
+    /* Assetions */
+    `ifdef SVA
+        // (1). If POS_DET_O is high,  NEG_DET_O must never be high in the same clock cycle.
+        sva_pos_neg_nand1: assert property (
+            @(posedge CLK_I) disable iff (!N_RST_I) (POS_DET_O |-> !NEG_DET_O)
+        );
+
+        // (2). If NEG_DET_O is high,  POS_DET_O must never be high in the same clock cycle.
+        sva_pos_neg_nand2: assert property (
+            @(posedge CLK_I) disable iff (!N_RST_I) (NEG_DET_O |-> !POS_DET_O)
+        );
+
+        // (3). POS_DET_O must be high for exactly one clock cycle only.
+        sva_pos_1cycle: assert property (
+            @(posedge CLK_I) disable iff (!N_RST_I) ($rose(POS_DET_O) |=> $fell(POS_DET_O))
+        );
+
+        // (4). NEG_DET_O must be high for exactly one clock cycle only.
+        sva_neg_1cycle: assert property (
+            @(posedge CLK_I) disable iff (!N_RST_I) ($rose(NEG_DET_O) |=> $fell(NEG_DET_O))
+        );
+
+        // (5). When POS_DET_O rises, DCLK_O shall rise simultaneously.
+        sva_dclk_pos: assert property (
+            @(posedge CLK_I) disable iff (!N_RST_I) ($rose(POS_DET_O) |-> $rose(DCLK_O))
+        );
+
+        // (6). When NEG_DET_O rises, DCLK_O shall fall simultaneously.
+        sva_dclk_neg: assert property (
+            @(posedge CLK_I) disable iff (!N_RST_I) ($rose(NEG_DET_O) |-> $fell(DCLK_O))
+        );
+    `endif // SVA
+
+endmodule
+
+`default_nettype wire
---
+++ 01_FIREEEE_DCLK_EDGE_DET/fireeee_dclk_edge_det_no_reset.v
@@ -0,0 +1,165 @@
+/*-----------------------------------------------------------------------------
+* fireeee_dclk_edge_det_no_reset.v
+*
+* Data Clock Edge Detector Instance (No Reset Version.)
+*
+* Version: 0.01
+* Author : AUDIY
+* Date   : 2026/03/09
+*
+* Port
+*   Input
+*       CLK_I  : Clock
+*       DCLK_I : Data Clock
+*       DATA_I : Data
+*
+*   Output
+*       DCLK_O   : Data Clock
+*       DATA_O   : Data
+*       POS_DET_O: Positive Edge Flag
+*       NEG_DET_O: Negative Edge Flag
+*
+* Parameters
+*   DATA_BIT_WIDTH : Data bit width (Default: 32)
+*   IN_REG_EN      : Input register enable (Default: 1'b1 = Enable)
+*   OUT_REG_EN     : Output register enable (Default: 1'b1 = Enable)
+*
+* License
+--------------------------------------------------------------------------------
+| Copyright AUDIY 2026.                                                        |
+|                                                                              |
+| This source describes Open Hardware and is licensed under the CERN-OHL-P v2. |
+|                                                                              |
+| You may redistribute and modify this source and make products using it under |
+| the terms of the CERN-OHL-P v2 (https:/cern.ch/cern-ohl).                    |
+|                                                                              |
+| This source is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,          |
+| INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A         |
+| PARTICULAR PURPOSE. Please see the CERN-OHL-P v2 for applicable conditions.  |
+--------------------------------------------------------------------------------
+*
+-----------------------------------------------------------------------------*/`default_nettype none
+
+module fireeee_dclk_edge_det_no_reset #(
+    parameter DATA_BIT_WIDTH = 32,
+    parameter IN_REG_EN      = 1'b1,
+    parameter OUT_REG_EN     = 1'b1
+) (
+    input  wire                          CLK_I    ,
+    input  wire                          DCLK_I   ,
+    input  wire [(DATA_BIT_WIDTH - 1):0] DATA_I   ,
+    output wire                          DCLK_O   ,
+    output wire [(DATA_BIT_WIDTH - 1):0] DATA_O   ,
+    output wire                          POS_DET_O,
+    output wire                          NEG_DET_O
+);
+    
+    /* Internal wire/reg */
+    wire                          DCLKI_W;
+    wire [(DATA_BIT_WIDTH - 1):0] DATAI_W;
+
+    reg                          DCLK_R = 1'b0;
+    reg [(DATA_BIT_WIDTH - 1):0] DATA_R = {(DATA_BIT_WIDTH){1'b0}};
+
+    reg POS_DET_R = 1'b0;
+    reg NEG_DET_R = 1'b0;
+
+    wire POS_DET_W;
+    wire NEG_DET_W;
+
+    wire                          DCLKO_W;
+    wire [(DATA_BIT_WIDTH - 1):0] DATAO_W;
+    wire                          POS_DET_O_W;
+    wire                          NEG_DET_O_W;
+
+    generate
+        if (IN_REG_EN == 1'b1) begin: genblk_inreg1_noreset
+            /* w/ Input Register */
+            reg                          DCLKI_R = 1'b0;
+            reg [(DATA_BIT_WIDTH - 1):0] DATAI_R = {(DATA_BIT_WIDTH){1'b0}};
+
+            always @(posedge CLK_I) begin
+                DCLKI_R <= DCLK_I;
+                DATAI_R <= DATA_I;
+            end
+
+            assign DCLKI_W = DCLKI_R;
+            assign DATAI_W = DATAI_R;
+        end else begin: genblk_inreg0_noreset
+            /* w/o Input Register */
+            assign DCLKI_W = DCLK_I;
+            assign DATAI_W = DATA_I;
+        end
+    endgenerate
+
+    always @(posedge CLK_I) begin
+        DCLK_R <= DCLKI_W;
+        DATA_R <= DATAI_W;
+    end
+
+    /* Edge detection */
+    assign POS_DET_W =   DCLKI_W  & (~DCLK_R); // Positive Edge
+    assign NEG_DET_W = (~DCLKI_W) &   DCLK_R;  // Negative Edge
+
+    always @(posedge CLK_I) begin
+        POS_DET_R <= POS_DET_W;
+        NEG_DET_R <= NEG_DET_W;
+    end
+
+    generate
+        if (OUT_REG_EN == 1'b1) begin: genblk_outreg1_noreset
+            /* w/ Output Register */
+            reg                           POS_DET_O_R = 1'b0;
+            reg                           NEG_DET_O_R = 1'b0;
+            reg                           DCLKO_R     = 1'b0;
+            reg [(DATA_BIT_WIDTH  - 1):0] DATAO_R     = {(DATA_BIT_WIDTH){1'b0}};
+
+            always @(posedge CLK_I) begin
+                POS_DET_O_R <= POS_DET_R;
+                NEG_DET_O_R <= NEG_DET_R;
+                DCLKO_R     <= DCLK_R   ;
+                DATAO_R     <= DATA_R   ;
+            end
+
+            assign DCLKO_W     = DCLKO_R    ;
+            assign DATAO_W     = DATAO_R    ;
+            assign POS_DET_O_W = POS_DET_O_R;
+            assign NEG_DET_O_W = NEG_DET_O_R;
+        end else begin: genblk_outreg0_noreset
+            /* w/o Output Register */
+            assign DCLKO_W     = DCLK_R   ;
+            assign DATAO_W     = DATA_R   ;
+            assign POS_DET_O_W = POS_DET_R;
+            assign NEG_DET_O_W = NEG_DET_R;
+        end
+    endgenerate
+
+    /* Output assign */
+    assign DCLK_O    = DCLKO_W    ;
+    assign DATA_O    = DATAO_W    ;
+    assign POS_DET_O = POS_DET_O_W;
+    assign NEG_DET_O = NEG_DET_O_W;
+
+    /* Formal Property Check (w/ sby) */
+    `ifdef FORMAL
+        always @(posedge CLK_I) begin
+            // (1). POS_DET_O and NEG_DET_O must never be high in the same clock cycle.
+            formal_POS_NEG_NAND: assert (!((POS_DET_O == 1'b1) && (NEG_DET_O == 1'b1)));
+
+            // (2). POS_DET_O must be high for exactly one clock cycle only.
+            formal_POS_1CYCLE: assert (!((POS_DET_O == 1'b1) && ($past(POS_DET_O) == 1'b1)));
+
+            // (3). NEG_DET_O must be high for exactly one clock cycle only.
+            formal_NEG_1CYCLE: assert (!((NEG_DET_O == 1'b1) && ($past(NEG_DET_O) == 1'b1)));
+
+            // (4). When POS_DET_O rises, DCLK_O shall rise simultaneously.
+            formal_DCLK_POS: assert ((POS_DET_O == 1'b0) || ($rose(POS_DET_O) && $rose(DCLK_O)));
+
+            // (5). When NEG_DET_O rises, DCLK_O shall fall simultaneously.
+            formal_DCLK_NEG: assert ((NEG_DET_O == 1'b0) || ($rose(NEG_DET_O) && $fell(DCLK_O))); 
+        end
+    `endif // FORMAL
+    
+endmodule
+
+`default_nettype wire
